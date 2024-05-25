bf88f6f4d750008c21296a5ddd1c8b15f64a0a23
Haroon <biodunhi@gmail.com>
2024-05-26 00:48:35 +0500

    first commit

---
+++ javid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int AmerAge, AmaraAge;
+//prompt the user to enter Amer"s age
+cout<<"Please enter Amers age";
+cin>>AmerAge;
+//prompt the user to enter Amara"s age
+cout<<"Please enter Amaras age";
+cin>>AmaraAge;
+//preform the test
+if (AmerAge > AmaraAge)
+cout << "Amer is older than Amara";
+}
